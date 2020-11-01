f190368657eeebd28cfee8eb62367a57edc4013e
Michael Standen <screaminghawk@gmail.com>
2020-11-02 09:38:28 +1300

    Role thresholds stored in db

--- src/commands/scores.test.js
+++ src/commands/scores.test.js
@@ -6,13 +6,6 @@ const mockMessage = {
 	reply: () => {},
 }
 
-test('gets correct score role', t => {
-	t.is('1st Circle', scores.getScoreRole(1)) // On boundary
-	t.is('2nd Circle', scores.getScoreRole(11)) // Above boundary
-	t.is('5th Circle', scores.getScoreRole(69420)) // Meme
-	t.is(null, scores.getScoreRole(0)) // None
-})
-
 test('gets correct bonus point amount', t => {
 	scores.initScores({}, false) // Disable bonus
 	t.is(0, scores.getBonusPoints(mockMessage, moment().subtract(1, 'hours')))
