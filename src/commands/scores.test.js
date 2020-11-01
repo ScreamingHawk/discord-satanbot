b653e3325e8bea8578f0ab1571dc90dd9c1cd294
Michael Standen <screaminghawk@gmail.com>
2020-11-01 20:07:41 +1300

    Add autorole #6

--- src/commands/scores.test.js
+++ src/commands/scores.test.js
@@ -2,16 +2,16 @@ const test = require('ava')
 const moment = require('moment')
 const scores = require('./scores')
 
+const mockMessage = {
+	reply: () => {},
+}
+
 test('gets correct score role', t => {
 	t.is('1st Circle', scores.getScoreRole(1)) // On boundary
 	t.is('2nd Circle', scores.getScoreRole(11)) // Above boundary
 	t.is('5th Circle', scores.getScoreRole(69420)) // Meme
 	t.is(null, scores.getScoreRole(0)) // None
 })
-
-const mockMessage = {
-	reply: () => {},
-}
 
 test('gets correct bonus point amount', t => {
 	scores.initScores({}, false) // Disable bonus
