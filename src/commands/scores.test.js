dec13fd59891acc676d717908b7df4fe08ee8649
Michael Standen <screaminghawk@gmail.com>
2020-11-02 09:41:50 +1300

    Bonus points 1 per 5 min of inactivity

--- src/commands/scores.test.js
+++ src/commands/scores.test.js
@@ -21,6 +21,6 @@ test('gets correct bonus point amount', t => {
 	t.is(0, scores.getBonusPoints(mockMessage, moment().subtract(1, 'minutes'))) // 1 minute
 	t.is(0, scores.getBonusPoints(mockMessage, moment().subtract(15, 'minutes'))) // Boundary
 	t.is(1, scores.getBonusPoints(mockMessage, moment().subtract(16, 'minutes'))) // 1 point
-	t.is(10, scores.getBonusPoints(mockMessage, moment().subtract(25, 'minutes'))) // 10 points
-	t.is(45, scores.getBonusPoints(mockMessage, moment().subtract(1, 'hours'))) // 1 hour
+	t.is(2, scores.getBonusPoints(mockMessage, moment().subtract(20, 'minutes'))) // 2 points
+	t.is(10, scores.getBonusPoints(mockMessage, moment().subtract(1, 'hours'))) // 1 hour
 })
