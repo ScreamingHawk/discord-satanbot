eec161ff1ab011cb8f98af797229f5be43c48aa6
Michael Standen <screaminghawk@gmail.com>
2020-11-03 11:21:31 +1300

    Help hides admin functions from non admins #15

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,26 +1,36 @@
 const Discord = require('discord.js')
+const discordUtil = require('./../util/discord')
 
-const HELP_SIMPLE = {
-	score: 'Display your current points',
-	give: 'Admin command to modify points',
-	leaderboard: 'Show the top 10 points leaders',
-	threshold: 'Set up the autoroles gained through points',
-}
-
-let bot, prefix, HELP_COMPLETE
+let bot, prefix, HELP_DETAILS
 
 const initHelp = (botArg, prefixArg) => {
 	bot = botArg
 	prefix = prefixArg
-	HELP_COMPLETE = {
-		score: `\`${prefix}score\`\nDisplay your current points`,
-		give: `\`${prefix}give @user 10\`\nAdmin command to modify points.\nCan give negative points to subtract`,
-		leaderboard: `\`${prefix}leaderboard\`\nShow the top 10 points leaders`,
-		threshold: `\`${prefix}threshold role_name 10\`\nSet the role provided to be awards at 10 points\nThis command is only available to admins`,
+	HELP_DETAILS = {
+		score: {
+			admin: false,
+			short: 'Display your current points',
+			detailed: `\`${prefix}score\`\nDisplay your current points`,
+		},
+		give: {
+			admin: true,
+			short: 'Modify someone\'s points',
+			detailed: `\`${prefix}give @user 10\`\nAdmin command to modify points.\nCan give negative points to subtract`,
+		},
+		leaderboard: {
+			admin: false,
+			short: 'Show the top 10 points leaders',
+			detailed: `\`${prefix}leaderboard\`\nShow the top 10 points leaders`,
+		},
+		threshold: {
+			admin: true,
+			short: 'Set up the autoroles gained through points',
+			detailed: `\`${prefix}threshold role_name 10\`\nSet the role provided to be awards at 10 points\nThis command is only available to admins`,
+		},
 	}
 }
 
-const showHelp = (message, args) => {
+const showHelp = async (message, args) => {
 	// Embedded help
 	const embed = new Discord.MessageEmbed()
 		.setTitle('Help')
@@ -28,18 +38,27 @@ const showHelp = (message, args) => {
 		.setColor(0xd82929)
 
 	const helpCommand = args[0]
+	const isAdmin = discordUtil.checkAdmin(message)
 
-	if (helpCommand && helpCommand in HELP_COMPLETE) {
+	if (helpCommand && helpCommand in HELP_DETAILS) {
 		// Show the help info for a single command
+		const helpDetail = HELP_DETAILS[helpCommand]
+		if (helpDetail.admin && !isAdmin) {
+			return message.reply(
+				`the ${helpCommand} command is only available for admins`,
+			)
+		}
 		embed.setDescription(`Here's some more info about ${helpCommand}`)
-		embed.addFields({ name: helpCommand, value: HELP_COMPLETE[helpCommand] })
+		embed.addFields({ name: helpCommand, value: helpDetail.details })
 	} else {
 		// Complete help
 		embed.setDescription(
 			`Here's all the things I can do!\nType \`${prefix}help command\` for more info about a command`,
 		)
-		for (const [command, value] of Object.entries(HELP_SIMPLE)) {
-			embed.addFields({ name: `${prefix}${command}`, value })
+		for (const [command, value] of Object.entries(HELP_DETAILS)) {
+			if (!(value.admin && !isAdmin)) {
+				embed.addFields({ name: `${prefix}${command}`, value: value.short })
+			}
 		}
 	}
 
